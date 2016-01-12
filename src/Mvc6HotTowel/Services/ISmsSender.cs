c395fac1ff0d88259b5fb029786a86bfe9b2ecd3
John Kennedy <develop@amdi.com>
2016-01-11 21:02:01 -0500

    initial

---
+++ src/Mvc6HotTowel/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mvc6HotTowel.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
